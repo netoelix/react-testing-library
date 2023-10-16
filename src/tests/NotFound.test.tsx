901974d5ecf1472204918176c34d5d9327072941
Elix Neto <neto.elix@outlook.com>
2023-10-16 18:39:57 -0300

    Finalizando o requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,20 @@
-test('', () => {});
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />, { wrapper: BrowserRouter });
+    const headingNotFound = screen.getByRole('heading', { level: 2 });
+
+    expect(headingNotFound).toHaveTextContent(/page requested not found/i);
+  });
+
+  test('Teste se a página mostra a imagem com o texto alternativo Clefairy pushing buttons randomly with text I have no idea what i\'m doing.', () => {
+    render(<NotFound />, { wrapper: BrowserRouter });
+    const image = screen.getByRole('img');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('alt', 'Clefairy pushing buttons randomly with text I have no idea what i\'m doing');
+  });
+});
