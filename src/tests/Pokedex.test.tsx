a8731f1f2b3e239f282e0d4d64d3ed13c61fd6d1
Elix Neto <neto.elix@outlook.com>
2023-10-16 19:38:59 -0300

    Finalizando requisito 5

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,51 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon.', () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const headingAbout = screen.getByRole('heading', { level: 2 });
+
+    expect(headingAbout).toHaveTextContent(/encountered pokémon/i);
+  });
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado.', async () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const btnNext = screen.getByTestId('next-pokemon');
+    expect(btnNext).toBeInTheDocument();
+
+    await userEvent.click(btnNext);
+    expect(screen.getByText(/charmander/i));
+
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+    await userEvent.click(btnNext);
+
+    expect(screen.getByText(/pikachu/i));
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const allBtn = screen.getAllByTestId(/pokemon-type-button/i);
+    expect(allBtn[0]).toHaveTextContent(/electric/i);
+    expect(allBtn[1]).toHaveTextContent(/fire/i);
+    expect(allBtn[2]).toHaveTextContent(/bug/i);
+    expect(allBtn[3]).toHaveTextContent(/poison/i);
+    expect(allBtn[4]).toHaveTextContent(/psychic/i);
+    expect(allBtn[5]).toHaveTextContent(/normal/i);
+    expect(allBtn[6]).toHaveTextContent(/dragon/i);
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro.', () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const btnReset = screen.getByText(/all/i);
+
+    expect(btnReset).toBeInTheDocument();
+  });
+});
