86927267bed86446e8bf6be1bab141778642d2aa
Elix Neto <neto.elix@outlook.com>
2023-10-16 18:06:24 -0300

    Corrigindo requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
 import { About } from '../pages';
 
 describe('Teste o componente <About.tsx />', () => {
@@ -26,7 +25,7 @@ describe('Teste o componente <About.tsx />', () => {
   test('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
     render(<About />, { wrapper: BrowserRouter });
 
-    const image = screen.getByRole('img', { name: /pokédex/i });
+    const image = screen.getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
 
     expect(image).toBeInTheDocument();
   });
